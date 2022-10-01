094e37cd84482ca43895cfb63dd90682f2be097c
adriano-ime <adriano.henrique@ime.eb.br>
2022-10-01 15:37:27 -0300

    :zap: Adding API to get values from microstack

--- api.py
+++ api.py
@@ -2,6 +2,8 @@
 from sqlite3 import connect
 import paramiko
 from config_util import get_config_values
+from parser import parse_microstack_table
+from objects import FlavorObject, ImageObject, NetworkObject, SecurityGroupObject
 from util import get_server_enum
 import os
 
@@ -12,11 +14,11 @@ def connect_to_host(selected_server):
     config = get_config_values()[server_name]
     ipaddress = config["ipaddress"]
     username = config["username"]
-    pwd = input("Insert host {} passsword: \n".format(ipaddress))
+    # pwd = input("Insert host {} passsword: \n".format(ipaddress))
     p = paramiko.SSHClient()
     p.set_missing_host_key_policy(paramiko.AutoAddPolicy())   # This script doesn't work for me unless this line is added!
-    print("Connecting to host {}, with username {} and password {}".format(ipaddress, username, pwd))
-    p.connect(ipaddress, port=VM_PORT, username=username, password=pwd)
+    print("Connecting to host {}, with username {} and password se9@ime_".format(ipaddress, username))
+    p.connect(ipaddress, port=VM_PORT, username=username, password="se9@ime_")
     print("Connection successful")
     return p
 
@@ -38,15 +40,55 @@ def open_vm(selected_server):
     command = "ssh {}@{} 'ls -l'".format(username, ipaddress)
     os.system("""osascript -e 'tell application "Terminal" to do script "{}"'""".format(command))
 
-def get_server_list(selected_server):
+def get_list_table(selected_server, command):
     p = connect_to_host(selected_server)
     print("Gathering information ...")
-    stdin, stdout, stderr = p.exec_command("microstack.openstack server list")
+    stdin, stdout, stderr = p.exec_command(command)
     opt = stdout.readlines()
     opt = "".join(opt)
+    print("Information succesfully retrieved")
+    print(opt)
     return opt
 
-s = get_server_list(1)
-# for element in s: 
-#     print(element)
-# print(s)
+def get_multiple_output_tables(selected_server, command_list):
+    p = connect_to_host(selected_server)
+    print("Gathering multiple information information ...")
+    opt_list = []
+    for command in command_list:
+        print("Running command ", command)
+        output = get_list_table(selected_server, command)
+        opt_list.append(output)
+    return opt_list
+
+#TODO: Turn all these methods into a single one
+def get_available_flavors(flavor_table):
+    flavor_list = parse_microstack_table(flavor_table)
+    flavor_object_list = []
+    for index in range(1, len(flavor_list)):
+        flavor_object = FlavorObject(flavor_list[index][1], flavor_list[index][2], flavor_list[index][3], flavor_list[index][4], flavor_list[index][5], flavor_list[index][6])
+        flavor_object_list.append(flavor_object)
+    return flavor_object_list
+
+def get_available_images(image_table):
+    image_list = parse_microstack_table(image_table)
+    image_object_list = []
+    for index in range(0, len(image_list)):
+        image_object = ImageObject(image_list[index][0], image_list[index][1],image_list[index][2])
+        image_object_list.append(image_object)
+    return image_object_list
+
+def get_available_networks(network_table):
+    network_list = parse_microstack_table(network_table)
+    network_object_list = []
+    for index in range(1,len(network_list)):
+        network_object = NetworkObject(network_list[index][0], network_list[index][1], network_list[index][2])
+        network_object_list.append(network_object)
+    return network_object_list
+
+def get_available_security_groups(security_group_table):
+    security_group_list = parse_microstack_table(security_group_table)
+    security_group_object_list = []
+    for index in range(1,len(security_group_list)):
+        security_group_object = SecurityGroupObject(security_group_list[index][0], security_group_list[index][1], security_group_list[index][2], security_group_list[index][3], security_group_list[index][4])
+        security_group_object_list.append(security_group_object)
+    return security_group_object_list
--- objects.py
+++ objects.py
@@ -6,3 +6,35 @@ class SeverObject():
         self.networks = networks
         self.image = image
         self.flavor = flavor
+
+class FlavorObject():
+    def __init__(self, name, ram, disk, ephemeral, vcpus, is_public):
+        self.name = name
+        self.ram = ram
+        self.disk = disk
+        self.ephemeral = ephemeral
+        self.vcpus = vcpus
+        self.is_public = is_public
+
+class ImageObject():
+    def __init__(self, id, name, status):
+        self.id = id
+        self.name = name
+        self.status = status
+
+
+class NetworkObject():
+    def __init__(self, id, name, subnets):
+        self.id = id
+        self.name = name
+        # TODO: Update the parser to consider mutliple subnets split by comma (,)
+        self.subnets = subnets
+    
+class SecurityGroupObject():
+    def __init__(self, id, name, description, project, tags):
+        self.id = id
+        self.name = name
+        self.description = description
+        self.project = project
+        # TODO: Update parser to consider arrays ([])
+        self.tags = tags
